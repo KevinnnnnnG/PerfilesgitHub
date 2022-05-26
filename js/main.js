718407baeb44c8b4f656aa8760a911e7d8b1115d
KevinnnnnnG <kgomezquiroga1234@gmail.com>
2022-05-26 17:33:51 -0500

    Mostrar información del usuarios en el github

--- js/main.js
+++ js/main.js
@@ -1,16 +1,36 @@
 const API = "https://api.github.com/users/";
 
-async function Buscar(){
-    const response = await fetch(API+"KevinnnnnnG")
-    //ahora quiero traer la info en formato json
-    const data = await response.json()
-    console.log(data)
-}
+// Se corto la funcion y se pega en el metodo
 
-const app = Vue.createApp({
+const app1 = Vue.createApp({
     data() {
         return {
-          message: 'Hola aprendiz'
+          message: 'Hola aprendiz',
+          busqueda : null,
+          result : null,
+          error : null
         }
+      },
+    // La palabra ya no es necesaria ya que se usa un meotod
+      methods: {
+        async Buscar(){
+          // esta supeditado depende del error
+          this.result = this.error = null
+          try {
+            const response = await fetch(API + this.busqueda)
+            if (!response.ok) throw new Error("Usuario no encontrado")
+            //console.log(response)
+            //ahora quiero traer la info en formato json
+            const data = await response.json()
+            console.log(data)
+            this.result = data //cambiar true por data
+          } catch (error) {
+            this.error = error
+          } 
+          // Tan pronto que termine el proceso , limpia haciendo vaica la busqueda 
+          finally {
+            this.busqueda = null
+          }
+        }        
       }
 }) // Montamos esta informacion en el html o en el div app
