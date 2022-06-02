b66eabdd181b5f1ef3606362a3697d037ee6f7f2
KevinnnnnnG <kgomezquiroga1234@gmail.com>
2022-06-02 18:28:51 -0500

    Se añadio la opcion de añadir y eliminar favoritos de los datos almacenados en la cache

--- js/main.js
+++ js/main.js
@@ -8,7 +8,29 @@ const app1 = Vue.createApp({
           message: 'Hola aprendiz',
           busqueda : null,
           result : null,
-          error : null
+          error : null,
+          favoritos: new Map() // Se creo un Map para guardar
+        }
+      },
+
+      created() {
+        const FavoritosGuardados = JSON.parse(window.localStorage.getItem("misfavoritos"))
+       /*if (FavoritosGuardados.length){
+          // recorremos el map con un nuevo nombre
+          const favoritosnew = new Map(FavoritosGuardados.map)
+       } */
+        //console.log(FavoritosGuardados)
+        //console.log("estoy en CREATED")
+      },
+
+      computed:{
+        estaFavoritos(){
+          return this.favoritos.has(this.result.id);
+        },
+        TodosFavoritos (){
+          // Pasamos la informaion a un autentico array
+          return Array.from(this.favoritos.values());
+          // el metodo values() traera los valores sin las clave
         }
       },
     // La palabra ya no es necesaria ya que se usa un meotod
@@ -31,6 +53,18 @@ const app1 = Vue.createApp({
           finally {
             this.busqueda = null
           }
-        }        
+        }, // Aqui se cerro el metodo buscar   
+          addFavorito(){
+            this.favoritos.set(this.result.id , this.result)
+            this.actualizarStorage()
+          },
+
+          RemoveraFavorito(){
+            this.favoritos.delete(this.result.id )
+            this.actualizarStorage()
+          },
+          actualizarStorage(){
+            window.localStorage.setItem('misfavoritos', JSON.stringify (this.TodosFavoritos))
+          }
       }
 }) // Montamos esta informacion en el html o en el div app
